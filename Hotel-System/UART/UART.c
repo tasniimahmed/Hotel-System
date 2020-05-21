53d67cb6bc6d0492cb5aa68b5e24a3988238f905
tasniimahmed <tasnim_eltaliawie@hotmail.com>
2020-05-21 03:51:05 +0200

    uart driver done

--- Hotel-System/UART/UART.c
+++ Hotel-System/UART/UART.c
@@ -1,10 +1,10 @@
 #include "UART.h"
 
 void UART_INIT(void){
-	SYSCTL_RCGCUART_R |= 0x0002;   //activate UART1
-	SYSCTL_RCGCGPIO_R |= 0x0002;   //activate port B
-	UART1_CTL_R &= ~0x0001;        //disbale UART
-	UART1_IBRD_R=104;              // for 9600 baudrate and clk=16 MHZ
+	SYSCTL_RCGCUART_R |= 0x0002;    //activate UART1
+	SYSCTL_RCGCGPIO_R |= 0x0002;    //activate port B
+	UART1_CTL_R &= ~0x0001;         //disbale UART
+	UART1_IBRD_R=104;               // for 9600 baudrate and clk=16 MHZ
 	UART1_FBRD_R=11;
 	UART1_LCRH_R = 0x0070;          //8 bits length & 1 stop bit & FIFO 16 bits
 	UART1_CTL_R = 0x0301;           //enable RXE and TXE
@@ -13,4 +13,16 @@ void UART_INIT(void){
 	GPIO_PORTB_DEN_R |= 0x03;         //B0 and B1
 	GPIO_PORTB_AMSEL_R &= ~0x03;      //Not analog
  
+}
+
+void UART_TRANS_CHAR(char x){
+	while((UART1_FR_R&UART_FR_TXFF) !=0);  //waiting for FIFO to be empty
+	UART1_DR_R=x;
+}
+
+char UART_REC_CHAR(void){
+	
+	while((UART1_FR_R&UART_FR_RXFE) !=0);
+	return UART1_DR_R;
+	
 }
--- Hotel-System/UART/UART.h
+++ Hotel-System/UART/UART.h
@@ -1,4 +1,6 @@
 #include <stdint.h>
 #include "../tm4c123gh6pm.h"
 
-void UART_INIT(void);
+void UART_INIT(void); 
+void UART_TRANS_CHAR(char x);
+char UART_REC_CHAR(void);
