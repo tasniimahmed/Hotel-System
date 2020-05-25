036efb8e59b549b987bc95a153d09caf61c03731
tasniimahmed <tasnim_eltaliawie@hotmail.com>
2020-05-25 21:13:55 +0200

    logic is debugged

--- Hotel-System/main.c
+++ Hotel-System/main.c
@@ -10,7 +10,7 @@
 void Turn_on_lock(void);
 void Turn_off_lock(void);
 uint16_t get_room_number(void);
-uint16_t* set_password(void);
+uint16_t* set_password(uint16_t *set_pw);
 uint16_t set_status(void);
 uint16_t valid( uint16_t* t_pw);
 
@@ -20,19 +20,24 @@ int main(){
 	uint16_t first_reservation_flag=0;
 	uint16_t room_status;
 	uint16_t *password_set;
+	uint16_t set_pw[4];
+	
 	
 	while(1){
 		uint16_t room_num= get_room_number();         //get room number
 		room_status= set_status();                    //room is now occupied
+		//room_status=1;
 		
 		
 		if (room_status == OCCUPIED){
 			if (first_reservation_flag == 0){
-				password_set =set_password();       //receptionist sets the pw
+				password_set =set_password(set_pw);       //receptionist sets the pw
 				first_reservation_flag = 1;
 			}
-			if(valid(password_set)) Turn_on_lock();
-			else Turn_off_lock();
+			else{
+				if(valid(password_set)) Turn_on_lock();
+				else Turn_off_lock();
+			}
 		}
 		
 		else if (room_status == CLEANING){
@@ -66,22 +71,25 @@ void Turn_off_lock(void){
 uint16_t get_room_number(void){
 	return UART_REC_CHAR();
 }
+
 uint16_t set_status(void){
 	return UART_REC_CHAR();
 }
-uint16_t* set_password(void){
-	static uint16_t password_set[4];
+
+uint16_t* set_password(uint16_t *set_pw){
 	for (int i=0; i<PASSWORD_SIZE; i++){
-		password_set[i]= UART_REC_CHAR();
+		set_pw[i]= UART_REC_CHAR();
+		//set_pw[i]=1;
 	}
-	return password_set;
+	return set_pw;
 }
+
 uint16_t valid(uint16_t* t_pw){
-	uint16_t pw[4];
 	
 	//getting password from the client by keypad and check if it's valid or not
 	for(int i=0; i<4; i++){
 		uint16_t pw= KeyPad_getPressedKey();
+		//uint16_t pw=0;
 		if(pw != t_pw[i]) return 0;
 	}
 	return 1;
